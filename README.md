cc68184c9b84635ddf6608dae2bad7bbd8ebcd4b
Lauren <Lauren.Wang@nordstrom.com>
2017-09-14 22:36:56 -0700

    lint fixes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Workfront Event Source
 
 Event ingestion via Workfront [Event Subscription API](https://support.workfront.com/hc/en-us/articles/115000135574-Event-Subscription-API).
+
+Notes
+    Create an S3 bucket for your deployments called workfront-event-source.serverless.deployment-bucket.<AWS account region>.
--- workfront-subscriptions/index.js
+++ workfront-subscriptions/index.js
@@ -1,5 +1,10 @@
 const fetch = require('node-fetch')
 
+const WF_CONSTANTS = {
+  objCodes: ['USER', 'PORT', 'PRGM', 'PROJ', 'TASK', 'OPTASK', 'TMPL', 'PTLSEC', 'PTLTAB', 'CMPY', 'DOCU', 'NOTE'],
+  eventTypes: ['CREATE', 'DELETE', 'UPDATE', 'SHARE'],
+}
+
 const impl = (apiKey, subscriptionsURL) => {
   const composeMessage =
     (objCode, objId, eventType, url, authToken) =>
@@ -27,9 +32,27 @@ const impl = (apiKey, subscriptionsURL) => {
     return fetch(`${subscriptionsURL}/${subscriptionId}`, options)
   }
 
+  const getPayloadSchema = (schemaFile) => {
+    if (schemaFile && WF_CONSTANTS.objCodes.indexOf(schemaFile) > -1) {
+      return require(`./schemas/${schemaFile}.json`) // eslint-disable-line global-require, import/no-dynamic-require
+    } else {
+      return null
+    }
+  }
+
+  const getEnvelopeSchema = () => require('./schemas/subscription-event.json') // eslint-disable-line global-require
+
+  const getObjCodes = () => WF_CONSTANTS.objCodes
+
+  const getEventTypes = () => WF_CONSTANTS.eventTypes
+
   return {
     subscribeToEvent,
     deleteSubscription,
+    getPayloadSchema,
+    getEnvelopeSchema,
+    getObjCodes,
+    getEventTypes,
   }
 }
 
